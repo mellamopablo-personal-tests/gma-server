eb54509c03eb9e728218e41e92beea96a1b8a7a3
Pablo Rodríguez <pabloviolin8@gmail.com>
2016-12-22 12:48:31 +0100

    Added build script

--- docs/api_project.js
+++ docs/api_project.js
@@ -8,7 +8,7 @@ define({
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2016-12-22T11:15:37.140Z",
+    "time": "2016-12-22T11:48:14.492Z",
     "url": "http://apidocjs.com",
     "version": "0.16.1"
   }
